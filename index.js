6dfe2b06a4f685f2945051cc881ddb776fc8d396
ManuelStanghe <manu2600k@gmail.com>
2026-05-13 21:58:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -313,14 +313,14 @@ builder.defineMetaHandler(async ({ type, id }) => {
                 const csHH = (currentStart.getUTCHours() + (isSky ? 2 : 0)).toString().padStart(2, '0');
                 const csMM = currentStart.getUTCMinutes().toString().padStart(2, '0');
 
-                releaseInfo = `In onda ora: ${epgText(info.current.title)} (${csHH}:${csMM})`;
+                releaseInfo = `▸ ${epgText(info.current.title)} (${csHH}:${csMM})`;
 
                 if (info.upcoming && info.upcoming.length > 0) {
-                    description = '**Prossimi programmi:**\n' + info.upcoming.map(p => {
+                    description = 'Prossimi programmi:\n' + info.upcoming.map(p => {
                         const start = parseEpgTime(p.$.start);
                         const sHH = (start.getUTCHours() + (isSky ? 2 : 0)).toString().padStart(2, '0');
                         const sMM = start.getUTCMinutes().toString().padStart(2, '0');
-                        return `◉ **${sHH}:${sMM}**  ${epgText(p.title)}`;
+                        return `◉ ${sHH}:${sMM}  ${epgText(p.title)}`;
                     }).join('\n');
                 }
             }
